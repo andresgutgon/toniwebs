c4144daef2d4cf1620d45d138fd312ffe4f0f2d0
andresgutgon <andresgutgon@gmail.com>
2022-07-03 09:45:24 +0200

    Deploy site with GitHub actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maestroyoficiantedeceremonias.com',
+  path: process.env.NODE_ENV === 'production' ? '/toniwebs' : null,
   integrations: [tailwind(), react()]
-});
+})
