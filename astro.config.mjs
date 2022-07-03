fb69e7504692bc2f5f459b67471d4073b2627452
andresgutgon <andresgutgon@gmail.com>
2022-07-03 09:57:36 +0200

    base config has to be a string

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maestroyoficiantedeceremonias.com',
-  base: process.env.NODE_ENV === 'production' ? '/toniwebs' : null,
+  base: process.env.NODE_ENV === 'production' ? '/toniwebs' : '',
   integrations: [tailwind(), react()]
 })
